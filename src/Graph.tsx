385804cf91856ed9e51d8d1e659532c3c1dcbe9c
Isaac <isaacmiles36@gmail.com>
2024-08-19 17:57:28 +0100

    Update Graph Component

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -35,19 +35,6 @@ class Graph extends Component<IProps, {}> {
     const elem = (document.getElementsByTagName(
       "perspective-viewer"
     )[0] as unknown) as PerspectiveViewerElement;
-    elem.setAttribute("view", "y_line");
-    elem.setAttribute("column-pivots", '["stock"]');
-    elem.setAttribute("row-pivots", '["timestamp"]');
-    elem.setAttribute("columns", '["top_ask_price"]');
-    elem.setAttribute(
-      "aggregates",
-      JSON.stringify({
-        stock: "distinct count",
-        top_ask_price: "avg",
-        top_bid_price: "avg",
-        timestamp: "distinct count",
-      })
-    );
 
     const schema = {
       stock: "string",
@@ -64,6 +51,14 @@ class Graph extends Component<IProps, {}> {
 
       // Add more Perspective configurations here.
       elem.load(this.table);
+      elem.setAttribute("view", "y_line");
+      elem.setAttribute("column-pivots", '["stock"]');
+      elem.setAttribute("row_pivots", '["timestamp"]');
+      elem.setAttribute("columns", '["top_ask_price"]');
+      elem.setAttribute(
+        "aggregates",
+        '{"stock":"distinct_count", "top_ask_price":"avg", "top_bid_price":"avg", "timestamp":"distinct_count"}'
+      );
     }
   }
 
